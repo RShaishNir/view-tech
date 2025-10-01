610a8b0e143e065545a8c7ba64bc077794bc7afc
RShaishNir <roix.shaish@gmail.com>
2025-10-01 20:56:16 +0300

    Delete about.tsx

--- about.tsx
+++
@@ -1,7 +0,0 @@
-export default function About(){
-  return (<div>
-    <h1>About View-Tech</h1>
-    <p className='small'>Proactive human monitoring, observation, and AI analytics to keep people and operations safe.</p>
-    <p className='small'>This is a placeholder; we will replace with your finalized About copy.</p>
-  </div>)
-}
