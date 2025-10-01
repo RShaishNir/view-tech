6178eb5f9bea419bc67f237b78dcc9ed02b26b15
RShaishNir <roix.shaish@gmail.com>
2025-10-01 20:57:22 +0300

    Delete insights.tsx

--- insights.tsx
+++
@@ -1,7 +0,0 @@
-export default function Insights(){
-  return (<div>
-    <h1>Insights</h1>
-    <p className='small'>Our perspectives on proactive human monitoring, observation, and AI analytics.</p>
-    <p className='small'>We will insert full articles + thumbnails next.</p>
-  </div>)
-}
